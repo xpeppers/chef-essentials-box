a794bd6ba1da138d461ce9b76a4b29ac109f920e
Filippo Liverani <fili_live@yahoo.it>
2015-12-14 10:26:09 +0100

    Moved course files to dedicated repo

--- chef-essentials/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VAGRANT_HOME=/c/HashiCorp/Vagrant
-VIRTUALBOX_HOME=/c/Program\ Files/Oracle/VirtualBox
-
-export PATH="${PATH}:${VAGRANT_HOME}/bin:${VIRTUALBOX_HOME}"
-
-vagrant box add --name xpeppers/chef-essentials chef-essentials.box
-vagrant up
