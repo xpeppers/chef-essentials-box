76709a0a67fe0d3cdb82bf598a73cf6125de59c0
Filippo Liverani <fili_live@yahoo.it>
2015-12-18 21:32:29 +0100

    Updated Dockerfile path and added some box slimming to provision script

--- provision.sh
+++ provision.sh
@@ -1,18 +1,37 @@
 #!/bin/sh
 
-yum install -y dkms gcc kernel-devel kernel-headers tree git nano vim emacs httpd
-service vboxadd setup
+yum install -y tree git nano vim emacs httpd
 
 /sbin/sysctl -w net.ipv4.ip_forward=1
 service docker restart
 
-cd /vagrant/docker
-docker rmi $(docker images -q)
+cd /vagrant
+docker rmi -f $(docker images -q)
 docker build -t xpeppers/chef-essentials ./
 
 yum clean all
 
-cat /dev/null > ~/.bash_history && history -c
+umount /vagrant
 
-#sudo dd if=/dev/zero of=/EMPTY bs=1M
-#sudo rm -f /EMPTY
+find /var/log -type f | while read f; do echo -ne '' > $f; done;
+rm -rf /usr/src/vboxguest* /usr/src/virtualbox-ose-guest* /usr/src/virtualbox-guest*
+rm -rf /usr/src/linux-headers*
+
+unset HISTFILE
+rm -f /root/.bash_history
+rm -f /home/vagrant/.bash_history
+
+set +e
+
+count=`df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}'`;
+count=$((count -= 1))
+dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count;
+rm /tmp/whitespace;
+
+count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
+count=$((count -= 1))
+dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count;
+rm /boot/whitespace;
+
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
