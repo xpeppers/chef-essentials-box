63dc24749077c770d9b896e6fa225738b3d80000
Filippo Liverani <fili_live@yahoo.it>
2016-03-18 17:33:28 +0100

    Added openbox and terminal

--- provision.sh
+++ provision.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-yum install -y tree git nano vim emacs httpd
+set -e
+
+echo '' > /etc/yum.conf
+
+yum install -y epel-release kernel-devel kernel-headers dkms tree git nano vim emacs httpd
+yum groupinstall -y "Development Tools"
+yum install -y xorg-x11-drv-evdev xorg-x11-drv-keyboard xorg-x11-drv-mouse xorg-x11-fonts-100dpi \
+  xorg-x11-server-Xorg xorg-x11-server-common xorg-x11-server-utils xorg-x11-xinit openbox Terminal
 
 /sbin/sysctl -w net.ipv4.ip_forward=1
 service docker restart
+
+echo "xrandr --output VGA-0 --auto --primary --mode 1024x768" > /home/vagrant/.xprofile
+echo "exec openbox-session" > /home/vagrant/.xinitrc
+echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' > /home/vagrant/.bash_profile
+mkdir -p  /home/vagrant/.config/openbox
+echo 'setxkbmap it && terminal &' > /home/vagrant/.config/openbox/autostart
+chown -R vagrant:vagrant /home/vagrant
 
 cd /vagrant
 docker rmi -f $(docker images -q)
@@ -11,6 +25,7 @@ docker build -t xpeppers/chef-essentials ./
 
 yum clean all
 
+cd /
 umount /vagrant
 
 find /var/log -type f | while read f; do echo -ne '' > $f; done;
